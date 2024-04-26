548d7e9c72c81c40bb8225f96287c59565305eb0
Mayonaisey <mayaelmosalamy@gmail.com>
2024-04-27 00:57:51 +0300

    correcting the form

--- productspage/productsScript.js
+++ productspage/productsScript.js
@@ -43,17 +43,15 @@ function displayProduct() {
   }
 }
 
-
-  var clothes = 
-    {
-        "products":[
+var clothes = {
+  products: [
     {
       gender: "male",
       type: "jeans",
       name: "780 Men's Jogger Jeans",
       image_path: "images/men/l_20241-s4fi43z8-m0u-93-75-91-182_a.webp",
       price: "999.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -61,7 +59,7 @@ function displayProduct() {
       name: "750 Slim Fit Men's Denim Trousers",
       image_path: "images/men/l_20232-w3dr37z8-h45_u.webp",
       price: "999.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -69,7 +67,7 @@ function displayProduct() {
       name: "770 Super Skinny Men's Denim",
       image_path: "images/men/l_20232-w3dr79z8-309_u.webp",
       price: "970.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -77,7 +75,7 @@ function displayProduct() {
       name: "Crew Neck Short Sleeve Men's T-shirt",
       image_path: "images/men/l_20232-w32977z8-lal-91-78-95-189_a.webp",
       price: "399.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -85,7 +83,7 @@ function displayProduct() {
       name: "Polo Neck Short Sleeve Patterned Pike Men's T-shirt",
       image_path: "images/men/l_20241-s4io96z8-lqq-98-78-92-187_a.webp",
       price: "499.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -93,7 +91,7 @@ function displayProduct() {
       name: "Crew Neck Long Sleeve Men's Sweatshirt",
       image_path: "images/men/l_20241-s41484z8-cvl-97-78-96-188_a.webp",
       price: "899.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -101,7 +99,7 @@ function displayProduct() {
       name: "Crew Neck Long Sleeve Men's Sweatshirt",
       image_path: "images/men/l_20241-s41484z8-v9v-97-78-96-188_a.webp",
       price: "899.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "male",
@@ -109,7 +107,7 @@ function displayProduct() {
       name: "Crew Neck Long Sleeve Men's Sweatshirt",
       image_path: "images/men/l_20241-s41484z8-ven-99-76-96-188_a.webp",
       price: "899.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -117,7 +115,7 @@ function displayProduct() {
       name: "Square Collar Straight Strap Poplin Women's Dress",
       image_path: "images/women/l_20231-s3ec25z8-rql-84-59-91-175_a.webp",
       price: "1.349.10",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -125,7 +123,7 @@ function displayProduct() {
       name: "Crew Neck Patterned Long Sleeve Women's Dress",
       image_path: "images/women/l_20241-s4hm69z8-lqq-83-59-90-174_a.webp",
       price: "899.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -133,7 +131,7 @@ function displayProduct() {
       name: "Crew Neck Printed Short Sleeve Women's T-shirt",
       image_path: "images/women/black_tshirt.webp",
       price: "249.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -141,7 +139,7 @@ function displayProduct() {
       name: "Crew Neck Printed Short Sleeve Women's T-shirt",
       image_path: "images/women/purple_tshirt.webp",
       price: "299.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -149,7 +147,7 @@ function displayProduct() {
       name: "Crew Neck Patterned Long Sleeve Women's Blouse",
       image_path: "images/women/l_20232-w3da40z8-h6z-87-60-90-175_a.webp",
       price: "419,40",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -157,7 +155,7 @@ function displayProduct() {
       name: "Boat Collar Straight Short Sleeve Women's Blouse",
       image_path: "images/women/white_blouse.webp",
       price: "639,20",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -165,7 +163,7 @@ function displayProduct() {
       name: "Boat Collar Straight Long Sleeve Women's Blouse",
       image_path: "images/women/l_20231-s3jm35z8-r9j-84-59-91-175_a.webp",
       price: "54.99",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -173,7 +171,7 @@ function displayProduct() {
       name: "Elastic Waist Patterned Women's Palazzo Trousers",
       image_path: "images/women/l_20241-s4au41z8-sw5-83-60-90-176_a.webp",
       price: "799.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -181,7 +179,7 @@ function displayProduct() {
       name: "Women's Elastic Waist Straight Knitwear Trousers",
       image_path: "images/women/set_trousers_beige.webp",
       price: "899,40",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -189,7 +187,7 @@ function displayProduct() {
       name: "Elastic Waist Comfortable Fit Muslin Women's Trousers",
       image_path: "images/women/black_trousers.webp",
       price: "799.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -197,7 +195,7 @@ function displayProduct() {
       name: "Single Band Women's Slippers",
       image_path: "images/women/l_20241-s4i215z8-j5e_a.webp",
       price: "499.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "female",
@@ -206,7 +204,7 @@ function displayProduct() {
       name: "Single Band Women's Slippers",
       image_path: "images/women/l_20241-s4i215z8-cr8_a.webp",
       price: "499.00",
-      size: ["S", "M", "L"]
+      size: ["S", "M", "L"],
     },
     {
       gender: "girls",
@@ -214,7 +212,7 @@ function displayProduct() {
       name: "U Neck Patterned Short Sleeve Girls' Dress",
       image_path: "images/kids/l_20231-s3bc30z4-lqq_a.webp",
       price: "769,30",
-      size: ["9", "10", "11", "12"]
+      size: ["9", "10", "11", "12"],
     },
     {
       gender: "girls",
@@ -222,7 +220,7 @@ function displayProduct() {
       name: "Square Neck Short Sleeve Girls Dress",
       image_path: "images/kids/l_20231-s3dq62z4-grm_a.webp",
       price: "1.189,30",
-      size: ["9", "10", "11", "12"]
+      size: ["9", "10", "11", "12"],
     },
     {
       gender: "girls",
@@ -230,7 +228,7 @@ function displayProduct() {
       name: "Kids Square Collar Girl Jean Salopet Dress",
       image_path: "images/kids/l_20232-w31234z4-h45-123-23_5.webp",
       price: "1099,00",
-      size: ["9", "10", "11", "12"]
+      size: ["9", "10", "11", "12"],
     },
     {
       gender: "girls",
@@ -238,7 +236,7 @@ function displayProduct() {
       name: "Crew Neck Basic Short Sleeve Girls T-Shirt",
       image_path: "images/kids/l_20241-s42363z4-g5t_a.webp",
       price: "149.00",
-      size: ["9", "10", "11", "12"]
+      size: ["9", "10", "11", "12"],
     },
     {
       gender: "girls",
@@ -246,7 +244,7 @@ function displayProduct() {
       name: "Crew Neck Basic Short Sleeve Girls T-Shirt",
       image_path: "images/kids/l_20241-s42363z4-cvl_a.webp",
       price: "149.00",
-      size: ["9", "10", "11", "12"]
+      size: ["9", "10", "11", "12"],
     },
     {
       gender: "girls",
@@ -254,58 +252,61 @@ function displayProduct() {
       name: "Crew Neck Basic Short Sleeve Girls T-Shirt",
       image_path: "images/kids/l_20241-s42363z4-grl_a.webp",
       price: "54.99",
-      size: ["9", "10", "11", "12"]
-    }
-  ]};
-  const productsList = clothes.products;
-  const productContainer = document.getElementById("product-container");
+      size: ["9", "10", "11", "12"],
+    },
+  ],
+};
+const productsList = clothes.products;
+const productContainer = document.getElementById("product-container");
 
-  productsList.forEach(function(product) {
-    // Create product div
-    var productDiv = document.createElement("div");
-    productDiv.classList.add("card");
+productsList.forEach(function (product) {
+  // Create product div
+  var productDiv = document.createElement("div");
+  productDiv.classList.add("card");
 
-    // Add product details
-    var image = document.createElement("img");
-    image.src = product.image_path;
-    productDiv.appendChild(image);
+  // Add product details
+  var image = document.createElement("img");
+  image.src = product.image_path;
+  productDiv.appendChild(image);
 
-    var name = document.createElement("p");
-    name.textContent = "Name: " + product.name;
-    productDiv.appendChild(name);
+  var name = document.createElement("p");
+  name.textContent = product.name;
+  productDiv.appendChild(name);
 
-    var gender = document.createElement("p");
-    gender.textContent = "Gender: " + product.gender;
-    productDiv.appendChild(gender);
+  var price = document.createElement("p");
+  price.textContent = "Price: " + product.price + " EGP";
+  productDiv.appendChild(price);
 
-    var price = document.createElement("p");
-    price.textContent = "Price: " + product.price+" EGP";
-    productDiv.appendChild(price);
+  var sizes = document.createElement("p");
+  sizes.textContent = "Sizes: " + product.size.join(", ");
+  productDiv.appendChild(sizes);
 
-    var sizes = document.createElement("p");
-    sizes.textContent = "Sizes: " + product.size.join(", ");
-    productDiv.appendChild(size);
-
-    // Append product div to container
-    productContainer.appendChild(productDiv);
-  });
+  // Append product div to container
+  productContainer.appendChild(productDiv);
+});
 function searchProduct() {
   if (document.getElementById("prod1").checked) {
-    productsList.innerHTML =
+    productContainer.innerHTML =
       document.getElementById("prod1").value + " radio button checked";
   } else if (document.getElementById("prod2").checked) {
-    productsList.innerHTML =
+    productContainer.innerHTML =
       document.getElementById("prod2").value + " radio button checked";
   } else if (document.getElementById("prod3").checked) {
-    productsList.innerHTML =
+    productContainer.innerHTML =
       document.getElementById("prod3").value + " radio button checked";
   } else if (document.getElementById("prod4").checked) {
-    productsList.innerHTML =
+    productContainer.innerHTML =
       document.getElementById("prod4").value + " radio button checked";
   } else if (document.getElementById("prod5").checked) {
-    productsList.innerHTML =
+    productContainer.innerHTML =
       document.getElementById("prod5").value + " radio button checked";
+  }else if (document.getElementById("prod6").checked) {
+    productContainer.innerHTML =
+      document.getElementById("prod6").value + " radio button checked";
+  }else if (document.getElementById("prod7").checked) {
+    productContainer.innerHTML =
+      document.getElementById("prod7").value + " radio button checked";
   } else {
-    productsList.innerHTML = "No one selected";
+    productContainer.innerHTML = "No one selected";
   }
 }
