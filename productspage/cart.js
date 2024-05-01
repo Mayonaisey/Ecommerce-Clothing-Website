15e882236b1e5cd4e0ebb7bee7bdb7898423c73e
Mayonaisey <mayaelmosalamy@gmail.com>
2024-05-01 16:51:43 +0300

    pushed

---
+++ productspage/cart.js
@@ -0,0 +1,3 @@
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+//make a cart array for every user , so the user must login first to add to cart
--- productspage/productsScript.js
+++ productspage/productsScript.js
@@ -36,239 +36,265 @@ function displayProduct() {
 
 var clothes = [
   {
-    id:"prod0",
+    id: "prod0",
     gender: "male",
     type: "jeans",
     name: "780 Men's Jogger Jeans",
     image_path: "images/men/l_20241-s4fi43z8-m0u-93-75-91-182_a.webp",
-    image_path2:"images/men/blackjeans.webp",
+    image_path2: "images/men/blackjeans.webp",
     price: "999.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod1",
+    id: "prod1",
     gender: "male",
     type: "jeans",
     name: "750 Slim Fit Men's Denim Trousers",
     image_path: "images/men/l_20232-w3dr37z8-h45_u.webp",
     price: "999.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod2",
+    id: "prod2",
     gender: "male",
     type: "jeans",
     name: "770 Super Skinny Men's Denim",
     image_path: "images/men/l_20232-w3dr79z8-309_u.webp",
     price: "970.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod3",
+    id: "prod3",
     gender: "male",
     type: "t-shirt",
     name: "Crew Neck Short Sleeve Men's T-shirt",
     image_path: "images/men/l_20232-w32977z8-lal-91-78-95-189_a.webp",
     price: "399.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod4",
+    id: "prod4",
     gender: "male",
     type: "t-shirt",
     name: "Polo Neck Short Sleeve Patterned Pike Men's T-shirt",
     image_path: "images/men/l_20241-s4io96z8-lqq-98-78-92-187_a.webp",
     price: "499.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod5",
+    id: "prod5",
     gender: "male",
     type: "sweatshirt",
     name: "Crew Neck Long Sleeve Men's Sweatshirt",
     image_path: "images/men/l_20241-s41484z8-cvl-97-78-96-188_a.webp",
     price: "899.00",
     size: ["S", "M", "L", "XL"],
+    quantity: 25,
   },
   {
-    id:"prod6",
+    id: "prod6",
     gender: "male",
     type: "sweatshirt",
     name: "Crew Neck Long Sleeve Men's Sweatshirt",
     image_path: "images/men/l_20241-s41484z8-v9v-97-78-96-188_a.webp",
     price: "899.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod7",
+    id: "prod7",
     gender: "male",
     type: "sweatshirt",
     name: "Crew Neck Long Sleeve Men's Sweatshirt",
     image_path: "images/men/l_20241-s41484z8-ven-99-76-96-188_a.webp",
     price: "899.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod8",
+    id: "prod8",
     gender: "female",
     type: "dress",
     name: "Square Collar Straight Strap Poplin Women's Dress",
     image_path: "images/women/l_20231-s3ec25z8-rql-84-59-91-175_a.webp",
     price: "1.349.10",
     size: ["S", "M", "L", "XL"],
+    quantity: 25,
   },
   {
-    id:"prod9",
+    id: "prod9",
     gender: "female",
     type: "dress",
     name: "Crew Neck Patterned Long Sleeve Women's Dress",
     image_path: "images/women/l_20241-s4hm69z8-lqq-83-59-90-174_a.webp",
     price: "899.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod10",
+    id: "prod10",
     gender: "female",
     type: "t-shirt",
     name: "Crew Neck Printed Short Sleeve Women's T-shirt",
     image_path: "images/women/black_tshirt.webp",
     price: "249.00",
     size: ["S", "M", "L", "XL"],
+    quantity: 25,
   },
   {
-    id:"prod11",
+    id: "prod11",
     gender: "female",
     type: "t-shirt",
     name: "Crew Neck Printed Short Sleeve Women's T-shirt",
     image_path: "images/women/purple_tshirt.webp",
     price: "299.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod12",
+    id: "prod12",
     gender: "female",
     type: "blouse",
     name: "Crew Neck Patterned Long Sleeve Women's Blouse",
     image_path: "images/women/l_20232-w3da40z8-h6z-87-60-90-175_a.webp",
     price: "419,40",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod13",
+    id: "prod13",
     gender: "female",
     type: "blouse",
     name: "Boat Collar Straight Short Sleeve Women's Blouse",
     image_path: "images/women/white_blouse.webp",
     price: "639,20",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod14",
+    id: "prod14",
     gender: "female",
     type: "blouse",
     name: "Boat Collar Straight Long Sleeve Women's Blouse",
     image_path: "images/women/l_20231-s3jm35z8-r9j-84-59-91-175_a.webp",
     price: "54.99",
     size: ["S", "M", "L", "XL"],
+    quantity: 25,
   },
   {
-    id:"prod15",
+    id: "prod15",
     gender: "female",
     type: "trousers",
     name: "Elastic Waist Patterned Women's Palazzo Trousers",
     image_path: "images/women/l_20241-s4au41z8-sw5-83-60-90-176_a.webp",
     price: "799.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod16",
+    id: "prod16",
     gender: "female",
     type: "trousers",
     name: "Women's Elastic Waist Straight Knitwear Trousers",
     image_path: "images/women/set_trousers_beige.webp",
     price: "899,40",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod17",
+    id: "prod17",
     gender: "female",
     type: "trousers",
     name: "Elastic Waist Comfortable Fit Muslin Women's Trousers",
     image_path: "images/women/black_trousers.webp",
     price: "799.00",
     size: ["S", "M", "L", "XL"],
+    quantity: 25,
   },
   {
-    id:"prod18",
+    id: "prod18",
     gender: "female",
     type: "slippers",
     name: "Single Band Women's Slippers",
     image_path: "images/women/l_20241-s4i215z8-j5e_a.webp",
     price: "499.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod19",
+    id: "prod19",
     gender: "female",
     type: "slippers",
     name: "Single Band Women's Slippers",
     image_path: "images/women/l_20241-s4i215z8-cr8_a.webp",
     price: "499.00",
     size: ["S", "M"],
+    quantity: 25,
   },
   {
-    id:"prod20",
+    id: "prod20",
     gender: "girls",
     type: "dress",
     name: "U Neck Patterned Short Sleeve Girls' Dress",
     image_path: "images/kids/l_20231-s3bc30z4-lqq_a.webp",
     price: "769,30",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod21",
+    id: "prod21",
     gender: "girls",
     type: "dress",
     name: "Square Neck Short Sleeve Girls Dress",
     image_path: "images/kids/l_20231-s3dq62z4-grm_a.webp",
     price: "1.189,30",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod22",
+    id: "prod22",
     gender: "girls",
     type: "dress",
     name: "Kids Square Collar Girl Jean Salopet Dress",
     image_path: "images/kids/l_20232-w31234z4-h45-123-23_5.webp",
     price: "1099,00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"pro23",
+    id: "pro23",
     gender: "girls",
     type: "t-shirt",
     name: "Crew Neck Basic Short Sleeve Girls T-Shirt",
     image_path: "images/kids/l_20241-s42363z4-g5t_a.webp",
     price: "149.00",
     size: ["S", "M"],
+    quantity: 25,
   },
   {
-    id:"prod24",
+    id: "prod24",
     gender: "girls",
     type: "t-shirt",
     name: "Crew Neck Basic Short Sleeve Girls T-Shirt",
     image_path: "images/kids/l_20241-s42363z4-cvl_a.webp",
     price: "149.00",
     size: ["S", "M", "L"],
+    quantity: 25,
   },
   {
-    id:"prod25",
+    id: "prod25",
     gender: "girls",
     type: "t-shirt",
     name: "Crew Neck Basic Short Sleeve Girls T-Shirt",
     image_path: "images/kids/l_20241-s42363z4-grl_a.webp",
     price: "54.99",
     size: ["S", "M"],
+    quantity: 25,
   },
 ];
 let products = clothes;
@@ -308,10 +334,13 @@ function makeCards(productsList) {
     btn.appendChild(sizes);
 
     // Append product div to container
-    btn.addEventListener("click", () =>{showItem( product.id);});
+    btn.addEventListener("click", () => {
+      showItem(product.id);
+    });
     productContainer.appendChild(btn);
     // j++;
   });
+  window.history.pushState({ id: 1 }, null, null);
 }
 makeCards(products);
 
@@ -369,81 +398,190 @@ function filterProducts() {
   }
 }
 
-function showItem(id) {  
-
- let number= parseInt( extractNumber(id));
+function showItem(id) {
+  
+  let number = parseInt(extractNumber(id));
   var product = document.createElement("div");
   product.classList.add("productDetails");
 
-    var photosContainer = document.createElement("div");
-    photosContainer.classList.add("photosContainer");
+  var photosContainer = document.createElement("div");
+  photosContainer.classList.add("photosContainer");
 
-    var image = document.createElement("img");
-    image.src =   clothes[number].image_path;
-    image.classList.add("image");
-    photosContainer.appendChild(image); 
-    var image2 = document.createElement("img");
-    image2.src =  clothes[number].image_path2;
-    image2.classList.add("image");
-    photosContainer.appendChild(image2); 
+  var image = document.createElement("img");
+  image.src = clothes[number].image_path;
+  image.classList.add("image");
+  photosContainer.appendChild(image);
+  var image2 = document.createElement("img");
+  image2.src = clothes[number].image_path2;
+  image2.classList.add("image");
+  photosContainer.appendChild(image2);
 
-    var details = document.createElement("div");
-    details.classList.add("details");
+  var details = document.createElement("div");
+  details.classList.add("details");
 
   var heartIcon = document.createElement("i");
-  heartIcon.innerHTML='<a href="wishlist.html"><i class="fa-regular fa-heart"></i></a>';
-  details.appendChild(heartIcon); 
+  heartIcon.innerHTML =
+    '<a href="wishlist.html"><i class="fa-regular fa-heart"></i></a>';
+  details.appendChild(heartIcon);
 
-    var name = document.createElement("p");
-    name.classList.add("name");
-    name.textContent=clothes[number].name;
-    details.appendChild(name);
+  var name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = clothes[number].name;
+  details.appendChild(name);
 
-    var gender = document.createElement("p");
-    gender.textContent=clothes[number].gender;
-    details.appendChild(gender);
+  var gender = document.createElement("p");
+  gender.textContent = clothes[number].gender;
+  details.appendChild(gender);
 
-    var itemSizes= document.createElement("p");
- itemSizes.classList.add("itemSizes");
-    itemSizes.innerHTML='<p>Sizes Available</p>';
-    for(let i=0; i<clothes[number].size.length; i++){
-      console.log(clothes[number].size[i]);
-      itemSizes.innerHTML+='<button class="sizesButtons">'+clothes[number].size[i]+'</var></button><br>';
-          //addEventListener
+  var itemSizes = document.createElement("p");
+  itemSizes.classList.add("itemSizes");
+  for (let i = 0; i < clothes[number].size.length; i++) {
+    console.log(clothes[number].size[i]);
+    var sizesButtons = document.createElement("button");
+    sizesButtons.textContent=clothes[number].size[i];
+    sizesButtons.classList.add("sizesButtons");
+    sizesButtons.id="size"+i;
+    sizesButtons.addEventListener("click", () => {
+      colorUp("size"+i);
+    });
+    itemSizes.appendChild(sizesButtons);
+  }
+  details.appendChild(itemSizes);
 
-    }
-    details.appendChild(itemSizes);
+  var quantityDiv = document.createElement("div");
+  quantityDiv.classList.add("quantityDiv");
+  var quantity = document.createElement("p");
+  quantity.textContent = "1";
+  quantity.id = "quantityID";
 
-    var price = document.createElement("p");
-    price.classList.add("itemPrice");
-    price.textContent=clothes[number].price;
-    details.appendChild(price);
+  var quantityBtn = document.createElement("button");
+  quantityBtn.innerHTML = '<i class="fa-solid fa-caret-up"></i>';
+  quantityBtn.classList.add("quantityBtn");
+  quantityBtn.addEventListener("click", () => {
+    incNumber("quantityID");
+  });
 
-    var addToCartBtn = document.createElement("button");
-    addToCartBtn.classList.add("addToCartBtn");
-    addToCartBtn.textContent="Add To Cart"
-    //addToCartBtn.addEventListener("click",incCartNumber());
-    details.appendChild(addToCartBtn);
+  var quantityDownBtn = document.createElement("button");
+  quantityDownBtn.innerHTML = '<i class="fa-solid fa-caret-down"></i>';
+  quantityDownBtn.classList.add("quantityBtn");
+  quantityDownBtn.addEventListener("click", () => {
+    decNumber("quantityID");
+  });
 
-    product.appendChild(photosContainer);
-    product.appendChild(details);
+  quantityDiv.appendChild(quantity);
+  quantityDiv.appendChild(quantityDownBtn);
+  quantityDiv.appendChild(quantityBtn);
+  details.appendChild(quantityDiv);
 
+  var price = document.createElement("p");
+  price.classList.add("itemPrice");
+  price.textContent = clothes[number].price + " EGP";
+  details.appendChild(price);
 
- //add a button for add to cart
-//append photos div and details div on productdetails and productdetails to page div
-let fullpage=document.getElementById("fullpage");
-fullpage.innerHTML="";
-fullpage.appendChild(product);
+  var addToCartBtn = document.createElement("button");
+  addToCartBtn.classList.add("addToCartBtn");
+  addToCartBtn.textContent = "Add To Cart";
+  addToCartBtn.addEventListener("click", () => {
+    incCartNumber("Cart-Quantity");
+  });
+  addToCartBtn.addEventListener("click", () => {
+    addToArray(number);
+  });
+  details.appendChild(addToCartBtn);
 
+  product.appendChild(photosContainer);
+  product.appendChild(details);
+
+  //append photos div and details div on productdetails and productdetails to page div
+  let fullpage = document.getElementById("fullpage");
+  fullpage.innerHTML = "";
+
+  // var continueShoppingDiv=document.createElement("div");
+  var continueShopping = document.createElement("button");
+  continueShopping.id="continueShopping";
+  continueShopping.textContent="Continue Shopping";
+  continueShopping.addEventListener("click", function(){
+    
+    window.open("productsPage.html", "_self");
+    printDetails();
+
+  });
+
+  // continueShopping.innerHTML='<a href="productsPage.html">Continue Shopping</a>';
+  fullpage.appendChild(continueShopping);
+  fullpage.appendChild(product);
 }
-
-function extractNumber(id){
+function extractNumber(id) {
   let str = id;
   let number = str.match(/\d+/g);
   return number;
 }
+function colorUp(id) {
+  let sizesButtons=document.getElementsByClassName("sizesButtons");
+  for(let i=0; i<sizesButtons.length; i++){
+    if(document.getElementById("size"+i).style.backgroundColor=="rgb(0, 58, 205)"){
+      document.getElementById("size"+i).style.backgroundColor="white"
+    }
+  }
+  document.getElementById(id).style.backgroundColor="rgb(0, 58, 205)";
+}
+
 // let counter = document.getElementById("counter")
 // counter.addEventListener("click", func);
-// function func(){
-//   counter.innerHTML=parseInt(counter.innerText)+1;
-// }
+function incNumber(id) {
+  var quantity = document.getElementById(id);
+  let number = parseInt(quantity.innerText);
+  quantity.innerHTML = number + 1;
+}
+function decNumber(id) {
+  var quantity = document.getElementById(id);
+  let number = parseInt(quantity.innerText);
+  if (number > 0) quantity.innerHTML = number - 1;
+}
+
+function incCartNumber(CartId) {
+  var quantitySelected = document.getElementById("quantityID");
+  let number = parseInt(quantitySelected.innerText);
+
+  var Cart = document.getElementById(CartId);
+  let CartQuantity = parseInt(Cart.innerText);
+  Cart.innerHTML = number + CartQuantity;
+}
+
+function addToArray(index){
+  let quantity=document.getElementById("quantityID");
+let number=parseInt(quantity.textContent);
+
+let sizesButtons=document.getElementsByClassName("sizesButtons");
+let sizeChosen="f";
+  for(let i=0; i<sizesButtons.length; i++){
+    if(document.getElementById("size"+i).style.backgroundColor=="rgb(0, 58, 205)"){
+      sizeChosen=document.getElementById("size"+i).textContent;
+      console.log(document.getElementById("size"+i).textContent +" is the size chosen");
+    }
+  }
+  
+  cart.push({
+    gender:clothes[index].gender ,size:sizeChosen,price:clothes[index].price,
+    type: clothes[index].type, name: clothes[index].name, quantity:number,image:clothes[index].image_path});
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function printDetails(){
+  let storedCart = JSON.parse(localStorage.getItem('cart'));
+  for(let i=0; i<storedCart.length; i++){
+    console.log(storedCart[i].gender + storedCart[i].size + storedCart[i].price + storedCart[i].type + storedCart[i].name + storedCart[i].quantity + storedCart[i].image);
+  }
+}
+let quantity=0;
+if(cart.length>0){
+    for(let i=0; i<cart.length; i++){
+        quantity+=cart[i].quantity;
+    }
+  var Cart = document.getElementById("Cart-Quantity");
+  Cart.innerHTML = quantity;
+}
+
+// window.addEventListener("popstate", detectHistory);
+// history.pushState(null, null, window.location.pathname);
+// function detectHistory() {}
