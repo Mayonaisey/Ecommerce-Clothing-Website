9f2edba4188257543b407d2030c80e882b8012e7
zaha hany <zahahani247@gmail.com>
2024-05-06 05:40:01 +0300

    changes
    changed the css in most of them and separated the js from the html and some changes in html

--- productspage/js-edit.js
+++ productspage/js-edit.js
@@ -1,7 +1,30 @@
+ user = {
+  /* username: usernameValue,
+   email: emailValue,
+   password: passwordValue,
+   password2: password2Value,
+   number: numberValue,
+   country: countryValue,
+   address: addressValue,
+   details: detailsValue,
+   isDefault: checkboxValue*/
+
+   username: "zaha",
+   email: "zaha@gmail.com",
+   password: "zaha123",
+   password2: "zaha123",
+   number: 1095921133,
+   country: "cairo",
+   address: "rehab city ",
+   details: "in front of gate 17"
+ /*  isDefault: checkboxValue*/
 
 
-console.log('file loaded');
-let xusername = document.getElementById('Eusername');
+ };
+
+  
+  function change(){
+    let xusername = document.getElementById('Eusername');
     let xemail = document.getElementById('Eemail');
     let xpassword = document.getElementById('Epassword');
     let xpassword2 = document.getElementById('Epassword2');
@@ -11,13 +34,9 @@ let xusername = document.getElementById('Eusername');
     let xdetails = document.getElementById('Edetails');
   
   
-  function change(){
-  
-    Console.log('function triggered');
     xusername.innerHTML = user.username;
     xemail.innerHTML = user.email;
     xpassword.innerHTML = user.password;
-    xpassword2.innerHTML = user.password2;
     xnumber.innerHTML = user.number;
     xcountry.innerHTML = user.country;
     xaddress.innerHTML = user.address;
@@ -25,4 +44,3 @@ let xusername = document.getElementById('Eusername');
 
   
   }
-
---
+++ productspage/signin.js
@@ -0,0 +1,19 @@
+
+
+
+function validateForm(event) {
+
+    //deh 3shan matkhleesh el page t refresh abl ma el js y execute :)
+
+  event.preventDefault(); 
+  
+    const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  
+  if (email === 'admin@gmail.com' && password === 'admin123') {
+    document.getElementById('usersButton').style.display = 'block';
+    document.getElementById('itemsButton').style.display = 'block';
+  } else {
+    alert('Incorrect email or password');
+  }
+}
